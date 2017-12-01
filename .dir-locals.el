6d0724a114b6e2655bac691dade09e91a5cc8f5d
Joe Jevnik <joe@quantopian.com>
2017-12-01 02:23:25 -0500

    DEV: c++17 and git-ignore

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,11 +2,13 @@
             (eval . (setq flycheck-gcc-include-path
                           '("../"
                             "../include"
-                            "../../include")))
+                            "../../include"
+                            "../../../include")))
             (eval . (c-set-offset 'innamespace 0))))
  (c++-mode . ((eval . (setq flycheck-gcc-include-path
                             '("../"
                               "../include"
-                              "../../include")))
+                              "../../include"
+                              "../../../include")))
               (eval . (c-set-offset 'innamespace 0))
-              (flycheck-gcc-language-standard . "gnu++14"))))
+              (flycheck-gcc-language-standard . "gnu++17"))))
