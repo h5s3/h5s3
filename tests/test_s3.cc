10884bc9126d11631e35c8cd872f30e73ed009a4
Scott Sanderson <ssanderson@quantopian.com>
2017-12-10 19:06:44 -0500

    TEST: Clean up s3 tests.
    - Dedupe common command code into a lambda.
    - Test multiple set/get pairs.
    - Add test for 404.

--- tests/test_s3.cc
+++ tests/test_s3.cc
@@ -3,6 +3,7 @@
 #include "gtest/gtest.h"
 
 #include "h5s3/s3.h"
+#include "h5s3/private/curl.h"
 #include "process.h"
 
 namespace fs = std::experimental::filesystem;
@@ -14,81 +15,85 @@ class S3Test : public ::testing::Test {
 public:
     static std::string ACCESS_KEY;
     static std::string SECRET_KEY;
+    static std::string TEST_BUCKET;
+    static std::string MINIO_ADDRESS;
+    static std::string REGION;
     static std::unique_ptr<process> minio_proc;
 
 protected:
 
     static void SetUpTestCase() {
         auto path = fs::temp_directory_path();
+        std::string minio_config = path / "minio-config/";
+
+        // Start minio server.
         std::vector<std::string> args = {
             "testbin/minio",
             "server",
-            "--config-dir", "testbin/minio-config/",
-            "--address", "localhost:9999",
-            "/tmp/minio/",
+            "--quiet",
+            "--config-dir", minio_config,
+            "--address", MINIO_ADDRESS,
+            path / "minio-data",
         };
         process::environment env = {
             {"MINIO_ACCESS_KEY", ACCESS_KEY},
             {"MINIO_SECRET_KEY", SECRET_KEY},
+            {"MINIO_REGION", REGION},
             {"PATH", std::getenv("PATH")},
         };
         minio_proc = std::make_unique<process>(args, env);
-        // HACK: Find a better way to determine that the minio server is up and
-        // running.
-        sleep(1);
+        sleep(1);  // Give it a second to come up.
 
-        std::vector<std::string> mc_args = {
-            "testbin/mc",
-            "--config-folder", "testbin/mc-config/", "--json",
-            "config",
-            "host",
-            "remove",
-            "h5s3-testing",
+        // Create a bucket with mc.
+        std::string mc_config = path / "mc-config/";
+        auto mc_command = [&mc_config](std::vector<std::string> extra){
+            std::vector<std::string> argv = {
+                "testbin/mc",
+                "--config-folder", mc_config,
+                "--quiet",
+            };
+            argv.insert(argv.end(), extra.begin(), extra.end());
+            process::environment env = {{"PATH", std::getenv("PATH")}};
+            process proc(argv, env);
+            auto result = proc.join();
+            EXPECT_EQ(result.code, 0);
         };
-        process::environment empty_env = {{"PATH", std::getenv("PATH")}};
-        process proc(mc_args, empty_env);
-        auto result = proc.join();
-        EXPECT_EQ(result.code, 0);
-
-        mc_args = {
-            "testbin/mc",
-            "--config-folder", "testbin/mc-config/", "--json",
-            "config",
-            "host",
-            "add",
-            "h5s3-testing",
-            "http://localhost:9999",
-            ACCESS_KEY, SECRET_KEY,
-        };
-        process proc2(mc_args, empty_env);
-        result = proc2.join();
-        EXPECT_EQ(result.code, 0);
-
-        mc_args = {
-            "testbin/mc",
-            "--config-folder", "testbin/mc-config/", "--json",
-            "mb", "--ignore-existing", "h5s3-testing/some-bucket",
-        };
-        process proc3(mc_args, empty_env);
-        result = proc3.join();
-        EXPECT_EQ(result.code, 0);
+        mc_command({"config", "host", "remove", "h5s3-testing"});
+        mc_command({"config", "host", "add", "h5s3-testing",
+                    "http://" + MINIO_ADDRESS, ACCESS_KEY, SECRET_KEY});
+        mc_command({"mb", "--ignore-existing",
+                    "--region", REGION,
+                    "h5s3-testing/" + TEST_BUCKET});
     }
 
     static void TearDownTestCase() {
         minio_proc->terminate();
         minio_proc->join();
-        minio_proc.release();
+        minio_proc.reset();
     }
 };
 
 std::string S3Test::ACCESS_KEY("TESTINGACCESSKEYXXXX");
 std::string S3Test::SECRET_KEY("TESTINGSECRETKEYXXXXXXXXXXXXXXXXXXXXXXXX");
+std::string S3Test::TEST_BUCKET("test-bucket");
+std::string S3Test::MINIO_ADDRESS("localhost:9999");
+std::string S3Test::REGION("us-west-2");
 std::unique_ptr<process> S3Test::minio_proc = nullptr;
 
+TEST_F(S3Test, set_then_get) {
+    s3::notary signer(REGION, ACCESS_KEY, SECRET_KEY);
 
-TEST_F(S3Test, test_set_get) {
-    s3::notary signer("us-east-1", ACCESS_KEY, SECRET_KEY);
-    s3::set_object(signer, "some-bucket", "foo/bar", "my-content", "localhost:9999", false);
-    std::string result = s3::get_object(signer, "some-bucket", "foo/bar", "localhost:9999", false);
-    EXPECT_EQ(result, "my-content");
+    auto key = "some/key";
+    for (auto content : {"content1", "content2", "content3"}) {
+        s3::set_object(signer, TEST_BUCKET, key, content, MINIO_ADDRESS, false);
+        std::string result = s3::get_object(signer, TEST_BUCKET, key, MINIO_ADDRESS, false);
+        EXPECT_EQ(result, content);
+    }
+}
+
+TEST_F(S3Test, get_nonexistent) {
+    s3::notary signer(REGION, ACCESS_KEY, SECRET_KEY);
+    auto key = "some/non/existent/key";
+
+    EXPECT_THROW({ s3::get_object(signer, TEST_BUCKET, key, MINIO_ADDRESS, false); }, h5s3::curl::http_error);
 }
