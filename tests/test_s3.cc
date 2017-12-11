89f0c32d0860dfb1cebcc34b9e0d83b2d59ae543
Scott Sanderson <ssanderson@quantopian.com>
2017-12-10 23:44:02 -0500

    TEST: Add tests for different buffer sizes.

--- tests/test_s3.cc
+++ tests/test_s3.cc
@@ -93,6 +93,96 @@ TEST_F(S3Test, set_then_get) {
     }
 }
 
+TEST_F(S3Test, get_into) {
+    std::array<char, 1024> content;
+    content.fill('X');
+
+    auto key = "get_into";
+    s3::set_object(notary,
+                   TEST_BUCKET,
+                   key,
+                   {content.data(), content.size()},
+                   MINIO_ADDRESS,
+                   false);
+
+    std::array<char, content.size()> outbuf_memory = {0};
+    h5s3::utils::out_buffer outbuf(outbuf_memory.data(), outbuf_memory.size());
+
+    std::size_t bytes_read = s3::get_object(outbuf,
+                                            notary,
+                                            TEST_BUCKET,
+                                            key,
+                                            MINIO_ADDRESS,
+                                            false);
+    ASSERT_EQ(bytes_read, content.size());
+    ASSERT_EQ(outbuf_memory, content);
+}
+
+TEST_F(S3Test, get_less_than_full) {
+    std::array<char, 512> content;
+    content.fill('X');
+
+    auto key = "get_less_than_full";
+    s3::set_object(notary,
+                   TEST_BUCKET,
+                   key,
+                   {content.data(), content.size()},
+                   MINIO_ADDRESS,
+                   false);
+
+    // Twice as much output memory as the content size.
+    std::array<char, content.size() * 2> outbuf_memory;
+    outbuf_memory.fill('Z');
+    h5s3::utils::out_buffer outbuf(outbuf_memory.data(), outbuf_memory.size());
+
+    std::size_t bytes_read = s3::get_object(outbuf,
+                                            notary,
+                                            TEST_BUCKET,
+                                            key,
+                                            MINIO_ADDRESS,
+                                            false);
+
+    // We should only get content_size bytes back.
+    ASSERT_EQ(bytes_read, content.size());
+    ASSERT_TRUE(bytes_read < outbuf.size());
+
+    // Initial bytes of the outbuf should match the expected content.
+    ASSERT_EQ(std::string_view(outbuf_memory.data(), content.size()),
+              std::string_view(content.data(), content.size()));
+
+    // Trailing bytes should hold their old value.
+    std::array<char, outbuf_memory.size() - content.size()> Zs;
+    Zs.fill('Z');
+    ASSERT_TRUE(std::equal(Zs.begin(),
+                           Zs.end(),
+                           outbuf_memory.begin() + content.size(),
+                           outbuf_memory.end()));
+}
+
+TEST_F(S3Test, get_more_than_full) {
+    std::array<char, 512> content;
+    content.fill('X');
+
+    auto key = "get_more_than_full";
+    s3::set_object(notary,
+                   TEST_BUCKET,
+                   key,
+                   {content.data(), content.size()},
+                   MINIO_ADDRESS,
+                   false);
+
+    // Outbuf not large enough to hold content.
+    std::array<char, content.size() - 1> outbuf_memory;
+    h5s3::utils::out_buffer outbuf(outbuf_memory.data(), outbuf_memory.size());
+    ASSERT_THROW({s3::get_object(outbuf,
+                                 notary,
+                                 TEST_BUCKET,
+                                 key,
+                                 MINIO_ADDRESS,
+                                 false); },
+        std::out_of_range);
+}
+
 TEST_F(S3Test, get_nonexistent) {
     auto key = "some/non/existent/key";
     EXPECT_THROW({ s3::get_object(notary, TEST_BUCKET, key, MINIO_ADDRESS, false); }, h5s3::curl::http_error);
